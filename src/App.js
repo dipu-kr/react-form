e57df1d3987aec555273bb67d2eab1e835c6bfa5
unknown <dipum906@gmail.com>
2021-07-28 19:41:50 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+function App(){
+    const [fullName,setFullName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [getName,setGetName] = useState();
+    const [getEmail,setGetEmail] = useState();
+    const [getPassword,setGetPassword] = useState();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const getData = (e) =>{
+          setFullName(e.target.value);
+    }
+
+    const getData2 = (e) =>{
+        setEmail(e.target.value);
+    }
+
+    const getData3 = (e) =>{
+        setPassword(e.target.value)
+    }
+    
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        setGetName(fullName);
+        setGetEmail(email);
+        setGetPassword(password);
+
+        setFullName("");
+        setEmail("");
+        setPassword("");
+    }
+    return(
+        <>
+        <div className="main">
+           <div className="child">
+             <form onSubmit={onSubmit}>
+                <p className="para1">Name : {getName}</p>
+                <p className="para2">Email : {getEmail}</p>
+                <p className="para3">Password : {getPassword}</p>
+                <input type="text" placeholder="Enter your First Name" value={fullName} onChange={getData}  autoFocus/>
+                <input type="email" placeholder="Enter your Email" value={email} onChange={getData2}/>
+                <input type="password" placeholder="Enter your Password" value={password} onChange={getData3}/>
+                <br/>
+                <button type="submit">Click Me</button>   
+             </form> 
+            </div>
+        </div>
+         </>
+    );
 }
 
 export default App;
